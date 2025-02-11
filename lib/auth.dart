46468d42adfb4413d5844f9e9777e1c2dd22b81d
ke3TAro2037 <116993117+ke3TAro2037@users.noreply.github.com>
2025-02-11 10:02:27 +0900

    Update auth.dart

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,3 +1,4 @@
+/*
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -88,3 +89,4 @@ class AuthService {
     }
   }
 }
+*/
